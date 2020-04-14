0e9fbc1e0b1619d2703da39568ee0e6cb24e4cf2
Sofya <sofya-good@mail.ru>
2020-04-14 16:26:57 +0200

    data preparation inside game logic

--- games/game-progression.js
+++ games/game-progression.js
@@ -2,42 +2,27 @@ import engine from '../src/engine.js';
 import getRandomInteger from '../src/getRandomInteger.js';
 
 
-const createQuestion = (progression, rightAnswer) => {
-  const question = [...progression];
-  question[progression.indexOf(rightAnswer)] = '..';
-
-  return question;
-};
-
-
 const rules = 'What number is missing in the progression?';
 
 const gameLogic = () => {
-  const getRandomProgression = (firstNumber, difference, quantity) => {
-    const randomProgression = [];
-    for (let i = 0; i < quantity; i += 1) {
-      const number = firstNumber + i * difference;
-      randomProgression.push(number);
-    }
-    return randomProgression;
-  };
-  
-  const getRandomProgressionMember = (progression) => {
-    const randomPosition = getRandomInteger(0, progression.length - 1);
-    const rightAnswer = progression[randomPosition];
-    return rightAnswer;
-  };
   const firstNumber = getRandomInteger(-50, 50);
   const difference = getRandomInteger(-5, 5);
-  const progression = getRandomProgression(firstNumber, difference, 10);
+  const progression = [];
+  
+  for (let i = 0; i < 10; i += 1) {
+    const number = firstNumber + i * difference;
+    progression.push(number);
+  }
 
-  const rightAnswer = getRandomProgressionMember(progression);
+  const randomPosition = getRandomInteger(0, progression.length - 1);
+  const rightAnswer = progression[randomPosition];
+  progression[randomPosition] = '..'
 
-  const question = createQuestion(progression, rightAnswer);
-  console.log(`Question: ${question.join(' ')}`);
+  const question = `Question: ${progression.join(' ')}`;
 
-  return String(rightAnswer);
+  return [String(rightAnswer), question];
 };
+
 
 const startFunction = () => {
   engine(rules, gameLogic);
